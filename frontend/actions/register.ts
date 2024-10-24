e01605192a3457d7315368f047d0c7b042ac8583
Max <m.paroshin@mail.ru>
2024-10-24 15:08:47 +0300

    refactor: update register route

--- frontend/actions/register.ts
+++ frontend/actions/register.ts
@@ -4,26 +4,28 @@ import * as z from "zod";
 
 import { RegisterSchema } from "@/schemas";
 
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+
+type ResponseData = {
+  detail: string;
+};
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-  try {
-    const response = await axios.postForm(
-      "http://localhost:8000/user/register/",
-      {
-        nickname: values.login,
-        password: values.password
+  const result = { success: true, message: "Вы успешно зарегестрировались!" };
+  await axios
+    .postForm("http://localhost:8000/user/register/", {
+      nickname: values.login,
+      password: values.password
+    })
+    .catch(function (error: AxiosError) {
+      result.success = false;
+      if (error.response) {
+        result.message = (error.response.data as ResponseData).detail;
+      } else if (error.request) {
+        result.message = "Пожалуйста повторите попытку позже";
+      } else {
+        result.message = "Неизвестная ошибка";
       }
-    );
-    if (response.status === 200) {
-      return { success: "Вы успешно зарегестрированы!" };
-    }
-    throw new Error(response.statusText);
-  } catch (error) {
-    if (error instanceof Error) {
-      return { error: error.message };
-    }
-    console.log(error);
-    throw error;
-  }
+    });
+  return result;
 };
